a9877b53dcf939a60415efdd0a2b9aea3a2cf2df
TheEZIC <35061249+TheEZIC@users.noreply.github.com>
2021-01-22 21:58:29 +0300

    Major changes

--- gulpfile.js
+++ gulpfile.js
@@ -1,28 +1,46 @@
+const { reload } = require('browser-sync');
+
 const 
     gulp         = require('gulp'),
     sass         = require('gulp-sass'),
     browserify   = require('gulp-browserify'),
-    rename       = require('gulp-rename');
+    rename       = require('gulp-rename'),
+    autoprefixer = require('gulp-autoprefixer'),
+    browserSync  = require('browser-sync').create();
 
-let modules = ['skills']
+let modules = [
+    'user',
+    'beatmapset',
+    'beatmap',
+    'top',
+    'recent',
+]
 
 gulp.task('default', () => {
-    modules.map(m => {
+    modules.map((m, i) => {
         gulp.task(`${m}Styles`, () => {
-            return gulp.src(`./cards/${m}/styles/*.scss`)
+            return gulp.src(`./cards/${m}/styles/index.scss`)
             .pipe(sass())
+            .pipe(autoprefixer())
             .pipe(gulp.dest(`./cards/${m}/` ))
-        })
+        });
         
         gulp.task(`${m}Scripts`, () => {
             return gulp.src(`./cards/${m}/scripts/*.js`)
             .pipe(browserify({ insertGlobals: true }))
             .pipe(rename('indexCompile.js'))
             .pipe(gulp.dest(`./cards/${m}/`))
-        })
+        });
         
-        gulp.watch(`./cards/${m}/styles/*.scss`, gulp.parallel(`${m}Styles`))
-        gulp.watch(`./cards/${m}/scripts/*.js`, gulp.parallel(`${m}Scripts`))
-        gulp.watch(`./cards/${m}/*.html`)
+        gulp.watch(`./cards/${m}/styles/*.scss`, gulp.parallel(`${m}Styles`));
+        gulp.watch(`./cards/${m}/scripts/*.js`, gulp.parallel(`${m}Scripts`));
+        gulp.watch(`./cards/${m}/*.html`);
     })
+});
+
+browserSync.init({
+    port: 3000,
+    server: {
+        baseDir: `./cards/top`,           
+    }
 });
